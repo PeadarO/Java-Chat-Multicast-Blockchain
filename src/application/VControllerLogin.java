3138a24d6360fd3512f1f650425dbb40feff96ce
Alejandro <Canarygo1@gmail.com>
2020-02-03 19:31:52 +0100

    user telephone empty

--- src/application/VControllerLogin.java
+++ src/application/VControllerLogin.java
@@ -10,41 +10,48 @@ import javafx.stage.Stage;
 import server.Server;
 
 public class VControllerLogin extends Controller {
-	private Server server;
-	@FXML
-	private TextField txtNumber;
-	@FXML
-	private TextField txtPassword;
-	@FXML
-	private Button btnLogin;
-	@FXML
-	private Button btnRegistrarUsuario;
 
-	public VControllerLogin() {
-		Server server = new Server();
-		this.server = server;
-	}
+    private Server server;
+    @FXML
+    private TextField txtNumber;
+    @FXML
+    private TextField txtPassword;
+    @FXML
+    private Button btnLogin;
+    @FXML
+    private Button btnRegistrarUsuario;
 
-	public void setServer(Server server) {
-		this.server = server;
-	}
+    public VControllerLogin() {
+        Server server = new Server();
+        this.server = server;
+    }
 
-	public void clickLogin(ActionEvent event) {
-		if (server.login(Integer.parseInt(txtNumber.getText()), txtPassword.getText())) {
-			if (server.isLogin()) {
-				System.out.println("LOGEADO");
-				// getusernameLogged(txtNumber.getText());
-				// mostrarVentana(event, (Node) event.getSource(), "App.fxml", "Chat Cifrado",
-				// false, true, -1);
-			} else {
-				dialog(AlertType.INFORMATION, "Information", "Error", "Bad Number phone or password");
-			}
-		} else {
-			dialog(AlertType.INFORMATION, "Information", "Error", "Please insert number and password");
-		}
-	}
+    public void setServer(Server server) {
+        this.server = server;
+    }
 
-	public void clickRegistro(ActionEvent event) {
-		cambiarVentana(event, (Stage) ((Node) event.getSource()).getScene().getWindow(), "Registro.fxml", "Registro");
-	}
+    public void clickLogin(ActionEvent event) {
+        if (txtNumber.getText().equals("")) {
+            dialog(AlertType.INFORMATION, "Information", "Error", "Telephone field is empty");
+
+            return;
+
+        }
+        if (server.login(Integer.parseInt(txtNumber.getText()), txtPassword.getText())) {
+            if (server.isLogin()) {
+                System.out.println("LOGEADO");
+                // getusernameLogged(txtNumber.getText());
+                // mostrarVentana(event, (Node) event.getSource(), "App.fxml", "Chat Cifrado",
+                // false, true, -1);
+            } else {
+                dialog(AlertType.INFORMATION, "Information", "Error", "Bad Number phone or password");
+            }
+        } else {
+            dialog(AlertType.INFORMATION, "Information", "Error", "Please insert number and password");
+        }
+    }
+
+    public void clickRegistro(ActionEvent event) {
+        cambiarVentana(event, (Stage) ((Node) event.getSource()).getScene().getWindow(), "Registro.fxml", "Registro");
+    }
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -23,230 +23,230 @@ import java.util.Properties;
 
 public class Server implements Interfaz, Remote {
 
-	private Connection connection;
-	private File config;
-	private Properties properties;
-	private InputStream input;
-	private OutputStream output;
-	private boolean login;
-	private int correctNumber;
+    private Connection connection;
+    private File config;
+    private Properties properties;
+    private InputStream input;
+    private OutputStream output;
+    private boolean login;
+    private int correctNumber;
 
-	public Server() {
-		config = new File("src/configuration.ini");
-		properties = new Properties();
-		output = null;
-		login = false;
-		getConnection();
-	}
+    public Server() {
+        config = new File("src/configuration.ini");
+        properties = new Properties();
+        output = null;
+        login = false;
+        getConnection();
+    }
 
-	// Conexion base de datos sql desde configuacion.ini
-	public Connection getConnection() {
-		String url = getProperty("url");
-		String user = getProperty("user");
-		String pass = getProperty("pass");
-		String driver = "com.mysql.cj.jdbc.Driver";
-		try {
-			System.out.println(user);
-			System.out.println(pass);
-			Class.forName(driver);
-			connection = DriverManager.getConnection(url, user, pass);
-		} catch (ClassNotFoundException e) {
-			System.out.println("ERROR: DRIVER NOT FOUND ");
-			System.exit(-1);
+    // Conexion base de datos sql desde configuacion.ini
+    public Connection getConnection() {
+        String url = getProperty("url");
+        String user = getProperty("user");
+        String pass = getProperty("pass");
+        String driver = "com.mysql.cj.jdbc.Driver";
+        try {
+            System.out.println(user);
+            System.out.println(pass);
+            Class.forName(driver);
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException e) {
+            System.out.println("ERROR: DRIVER NOT FOUND ");
+            System.exit(-1);
 
-		} catch (SQLException e) {
-			System.out.println("ERROR: CONNECTION FAILURE OF DATABASE" + e);
-			System.exit(-1);
-		} catch (Exception e) {
-			System.out.println("ERROR: UNKNOWN");
-			e.printStackTrace();
-			System.exit(-1);
-		}
-		return connection;
-	}
+        } catch (SQLException e) {
+            System.out.println("ERROR: CONNECTION FAILURE OF DATABASE" + e);
+            System.exit(-1);
+        } catch (Exception e) {
+            System.out.println("ERROR: UNKNOWN");
+            e.printStackTrace();
+            System.exit(-1);
+        }
+        return connection;
+    }
 
-	public String getProperty(String key) {
-		input = null;
-		try {
-			input = new FileInputStream(config);
-			properties.load(input);
-		} catch (Exception e) {
-			System.out.println("CONFIGURATION READING FAILURE (.INI)" + e);
-		}
-		String property = properties.getProperty(key);
-		return property;
-	}
+    public String getProperty(String key) {
+        input = null;
+        try {
+            input = new FileInputStream(config);
+            properties.load(input);
+        } catch (Exception e) {
+            System.out.println("CONFIGURATION READING FAILURE (.INI)" + e);
+        }
+        String property = properties.getProperty(key);
+        return property;
+    }
 
-	public void setProperty(String key, String value) {
-		properties.setProperty(key, value);
-		try {
-			output = new FileOutputStream(config);
-			properties.store(output, "");
+    public void setProperty(String key, String value) {
+        properties.setProperty(key, value);
+        try {
+            output = new FileOutputStream(config);
+            properties.store(output, "");
 
-		} catch (IOException e) {
-			System.out.println("Algo sali� mal");
-		}
-	}
+        } catch (IOException e) {
+            System.out.println("Algo sali� mal");
+        }
+    }
 
-	// LOGIN-REGISTRO
-	@Override
-	public boolean registerUser(int number, String username, String password, String name) {
-		String query = "INSERT INTO users (number,username, password, name,status) VALUES (?, ?, ?, ?,?)";
-		try {
-			PreparedStatement stmt = connection.prepareStatement(query);
-			stmt.setInt(1, number);
-			stmt.setString(2, username);
-			stmt.setString(3, cipher(password));
-			stmt.setString(4, name);
-			// when register status = false
-			stmt.setBoolean(5, false);
-			stmt.executeUpdate();
-			stmt.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return false;
-	}
+    // LOGIN-REGISTRO
+    @Override
+    public boolean registerUser(int number, String username, String password, String name) {
+        String query = "INSERT INTO users (number,username, password, name,status) VALUES (?, ?, ?, ?,?)";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(query);
+            stmt.setInt(1, number);
+            stmt.setString(2, username);
+            stmt.setString(3, cipher(password));
+            stmt.setString(4, name);
+            // when register status = false
+            stmt.setBoolean(5, false);
+            stmt.executeUpdate();
+            stmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 
-	// Comprueba si existe un usuario
-	public boolean existeUsuario(int number) throws SQLException {
-		String query = "SELECT number FROM users WHERE number LIKE ?";
-		PreparedStatement stmt = connection.prepareStatement(query);
-		stmt.setInt(1, number);
-		ResultSet rset = stmt.executeQuery();
-		boolean exist = rset.next();
-		rset.close();
-		stmt.close();
-		if (exist) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+    // Comprueba si existe un usuario
+    public boolean existeUsuario(int number) throws SQLException {
+        String query = "SELECT number FROM users WHERE number LIKE ?";
+        PreparedStatement stmt = connection.prepareStatement(query);
+        stmt.setInt(1, number);
+        ResultSet rset = stmt.executeQuery();
+        boolean exist = rset.next();
+        rset.close();
+        stmt.close();
+        if (exist) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	@Override
-	public boolean updateUser(int number, String username, String password, String name) throws RemoteException {
-		boolean state;
+    @Override
+    public boolean updateUser(int number, String username, String password, String name) throws RemoteException {
+        boolean state;
 
-		String query = "UPDATE users SET password = ?, name = ? WHERE number LIKE '" + number + "'";
-		try {
-			PreparedStatement stmt = connection.prepareStatement(query);
+        String query = "UPDATE users SET password = ?, name = ? WHERE number LIKE '" + number + "'";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(query);
 
-			stmt.setString(1, password);
-			stmt.setString(2, name);
-			stmt.executeUpdate();
-			stmt.close();
-			state = true;
-		} catch (SQLException e) {
-			e.printStackTrace();
-			state = false;
-		}
-		return state;
-	}
+            stmt.setString(1, password);
+            stmt.setString(2, name);
+            stmt.executeUpdate();
+            stmt.close();
+            state = true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            state = false;
+        }
+        return state;
+    }
 
-	@Override
-	public boolean deleteUser(int number) throws RemoteException {
-		try {
-			String query = "DELETE FROM users WHERE number LIKE ?";
-			PreparedStatement stmt = connection.prepareStatement(query);
-			stmt.setInt(1, number);
-			stmt.executeUpdate();
-			stmt.close();
-			return true;
-		} catch (Exception e) {
-			System.out.println(e);
-			return false;
-		}
+    @Override
+    public boolean deleteUser(int number) throws RemoteException {
+        try {
+            String query = "DELETE FROM users WHERE number LIKE ?";
+            PreparedStatement stmt = connection.prepareStatement(query);
+            stmt.setInt(1, number);
+            stmt.executeUpdate();
+            stmt.close();
+            return true;
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
 
-	}
+    }
 
-	@Override
-	public boolean login(int number, String password) {
-		correctNumber = 0;
-		String correctPassword = " ";
+    @Override
+    public boolean login(int number, String password) {
+        correctNumber = 0;
+        String correctPassword = " ";
 
-		try {
-			String query = "SELECT number, password FROM users WHERE number = ?  AND password = ?";
-			PreparedStatement stmt = connection.prepareStatement(query);
-			stmt.setInt(1, number);
-			stmt.setString(2, cipher(password));
-			ResultSet rset = stmt.executeQuery();
-			rset.next();
-			correctNumber = rset.getInt(1);
-			correctPassword = rset.getString(2);
-			rset.close();
-			stmt.close();
-		} catch (SQLException e) {
-			login = false;
-		}
+        try {
+            String query = "SELECT number, password FROM users WHERE number = ?  AND password = ?";
+            PreparedStatement stmt = connection.prepareStatement(query);
+            stmt.setInt(1, number);
+            stmt.setString(2, cipher(password));
+            ResultSet rset = stmt.executeQuery();
+            rset.next();
+            correctNumber = rset.getInt(1);
+            correctPassword = rset.getString(2);
+            rset.close();
+            stmt.close();
+        } catch (SQLException e) {
+            login = false;
+        }
 
-		login = correctNumber == number && correctPassword.equals(cipher(password));
-		if (login) {
-			// status == true
-			return true;
-		} else {
-			return false;
-		}
+        login = correctNumber == number && correctPassword.equals(cipher(password));
+        if (login) {
+            // status == true
+            return true;
+        } else {
+            return false;
+        }
 
-	}
+    }
 
-	private String cipher(String password) {
-		MessageDigest md;
-		String strCipherPwd = "";
-		try {
-			md = MessageDigest.getInstance("SHA-256");
-			byte[] cipherPwd = md.digest(password.getBytes(StandardCharsets.UTF_8));
+    private String cipher(String password) {
+        MessageDigest md;
+        String strCipherPwd = "";
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+            byte[] cipherPwd = md.digest(password.getBytes(StandardCharsets.UTF_8));
 
-			strCipherPwd = bytesToHex(cipherPwd);
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+            strCipherPwd = bytesToHex(cipherPwd);
+        } catch (NoSuchAlgorithmException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
-		return strCipherPwd;
-	}
+        return strCipherPwd;
+    }
 
-	private String bytesToHex(byte[] hashInBytes) {
+    private String bytesToHex(byte[] hashInBytes) {
 
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < hashInBytes.length; i++) {
-			sb.append(Integer.toString((hashInBytes[i] & 0xff) + 0x100, 16).substring(1));
-		}
-		return sb.toString();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < hashInBytes.length; i++) {
+            sb.append(Integer.toString((hashInBytes[i] & 0xff) + 0x100, 16).substring(1));
+        }
+        return sb.toString();
 
-	}
+    }
 
-	public boolean isLogin() {
-		return login;
-	}
+    public boolean isLogin() {
+        return login;
+    }
 
-	public void logout() {
-		connection = null;
-		login = false;
-		correctNumber = 0;
-	}
+    public void logout() {
+        connection = null;
+        login = false;
+        correctNumber = 0;
+    }
 
-	// MAIN
-	public static void main(String[] args) {
-		Registry reg = null;
-		try {
-			System.out.println("Creando el registro de objetos,escuchando en el puerto 5557");
-			reg = LocateRegistry.createRegistry(5557);
-		} catch (Exception e) {
-			System.out.println("ERROR:No se ha podido crear el registro");
-			e.printStackTrace();
-		}
-		System.out.println("Creando el objeto servidor");
-		Server serverObject = new Server();
-		try {
-			System.out.println("Inscribiendo el objeto servidor en el registro");
-			System.out.println("");
-			System.out.println("Se le da un nombre unico: Chat");
-			reg.rebind("Chat", (Interfaz) UnicastRemoteObject.exportObject(serverObject, 0));
-		} catch (Exception e) {
+    // MAIN
+    public static void main(String[] args) {
+        Registry reg = null;
+        try {
+            System.out.println("Creando el registro de objetos,escuchando en el puerto 5557");
+            reg = LocateRegistry.createRegistry(5557);
+        } catch (Exception e) {
+            System.out.println("ERROR:No se ha podido crear el registro");
+            e.printStackTrace();
+        }
+        System.out.println("Creando el objeto servidor");
+        Server serverObject = new Server();
+        try {
+            System.out.println("Inscribiendo el objeto servidor en el registro");
+            System.out.println("");
+            System.out.println("Se le da un nombre unico: Chat");
+            reg.rebind("Chat", (Interfaz) UnicastRemoteObject.exportObject(serverObject, 0));
+        } catch (Exception e) {
 
-			System.out.println("ERROR:No se ha podido inscribir el objeto servidor.");
-			e.printStackTrace();
-		}
-	}
+            System.out.println("ERROR:No se ha podido inscribir el objeto servidor.");
+            e.printStackTrace();
+        }
+    }
 }
